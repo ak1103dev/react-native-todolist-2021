f1a4635d6c6ad8747678db7ee08e574560ffb145
ak1103dev <ak.apichan1103@gmail.com>
2021-03-20 17:25:53 +0700

    :sparkles: firestore

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,76 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Text, TextInput, Button, TouchableOpacity} from 'react-native';
+import firestore from '@react-native-firebase/firestore';
+
+const tasksCollection = firestore().collection('Tasks');
 
 const HomeScreen = () => {
+  const [input, setInput] = useState('');
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    const subscriber = tasksCollection.onSnapshot(querySnapshot => {
+      console.log('Total users: ', querySnapshot.size);
+      const tempList = [];
+      querySnapshot.forEach(documentSnapshot => {
+        console.log('User ID: ', documentSnapshot.id, documentSnapshot.data());
+        tempList.push({
+          id: documentSnapshot.id,
+          ...documentSnapshot.data(),
+        });
+      });
+      setList(tempList);
+    });
+
+    // Stop listening for updates when no longer required
+    return () => subscriber();
+  }, []);
+
+  const addTask = () => {
+    console.log(input);
+    tasksCollection.add({
+      title: input,
+      isCompleted: false,
+    });
+    setInput('');
+  };
+
+  const updateTask = payload => {
+    tasksCollection.doc(payload.id).update({
+      isCompleted: !payload.isCompleted,
+      updatedAt: new Date(),
+    });
+  };
+
+  const completedTasks = list.filter(item => item.isCompleted);
+  const incompletedTasks = list.filter(item => !item.isCompleted);
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
+    <View style={{flex: 1, justifyContent: 'center'}}>
+      <Text>To Do List</Text>
+      <TextInput
+        value={input}
+        onChangeText={setInput}
+        placeholder="New Task"
+        style={{borderColor: '#d0d0d0', borderWidth: 1, marginBottom: 10}}
+      />
+      <Button title="Add" onPress={addTask} />
+      <Text style={{fontSize: 30}}>To Do</Text>
+      <View>
+        {incompletedTasks.map(item => (
+          <TouchableOpacity key={item.id} onPress={() => updateTask(item)}>
+            <Text>{item.title}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+      <Text style={{fontSize: 30}}>Done</Text>
+      <View>
+        {completedTasks.map(item => (
+          <TouchableOpacity key={item.id} onPress={() => updateTask(item)}>
+            <Text>{item.title}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
     </View>
   );
 };
